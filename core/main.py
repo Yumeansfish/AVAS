2a4929bb1182a150b7d838b094afa9db3bcaf81f
Chengyu Yu <nightnight2024@gmail.com>
2025-06-02 23:14:24 +0200

    Env.

--- core/main.py
+++ core/main.py
@@ -1,6 +1,12 @@
 import signal
 import sys
 import time
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = Path(__file__).parent
+load_dotenv(BASE_DIR / ".env")
 
 from core.config import WATCH_DIR
 from core.monitor import MonitorCore
